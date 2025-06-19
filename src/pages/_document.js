8a067a5ce647d108f673a32af5998f1f0bf91576
HJPATEL51 <savaliyahiren809@gmail.com>
2025-06-20 00:04:47 +0530

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Hiren | Portfolio</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
